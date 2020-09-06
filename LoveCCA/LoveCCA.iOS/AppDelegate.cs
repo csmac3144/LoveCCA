35b78f1313f429cd560fb13b400d32a5ff3bf8d4
Steve Macdonald <Steve.Macdonald@mnp.ca>
2020-09-06 10:23:16 -0300

    working on iOS push

--- LoveCCA/LoveCCA.iOS/AppDelegate.cs
+++ LoveCCA/LoveCCA.iOS/AppDelegate.cs
@@ -1,7 +1,10 @@
 ﻿
+using Firebase.CloudMessaging;
 using Foundation;
 using LoveCCA.Services;
+using System;
 using UIKit;
+using UserNotifications;
 
 namespace LoveCCA.iOS
 {
@@ -9,8 +12,12 @@ namespace LoveCCA.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate, IMessagingDelegate
     {
+        public void DidRefreshRegistrationToken(Messaging messaging, string fcmToken)
+        {
+            System.Diagnostics.Debug.WriteLine($"FCM Token: {fcmToken}");
+        }
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -29,6 +36,32 @@ namespace LoveCCA.iOS
             global::Xamarin.Forms.DependencyService.RegisterSingleton<IOrderCalendarService>(new OrderCalendarService());
             LoadApplication(new App());
             Firebase.Core.App.Configure();
+
+            // Register your app for remote notifications.
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // iOS 10 or later
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) => {
+                    Console.WriteLine(granted);
+                });
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+
+                // For iOS 10 data message (sent via FCM)
+                Messaging.SharedInstance.Delegate = this;
+            }
+            else
+            {
+                // iOS 9 or before
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
             return base.FinishedLaunching(app, options);
         }
     }
